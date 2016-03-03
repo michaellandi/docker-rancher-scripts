2deb000531e10760f5a1881304e3e31fb4b531ec
Karim Vaes <sourcetree@kvaes.be>
2016-03-03 16:32:10 +0100

    wait for docker installation, due to apt locking issues

--- agent-with-local-ip/rancher.sh
+++ agent-with-local-ip/rancher.sh
@@ -18,10 +18,23 @@ echo "# Setting Variables"
 HOST_URL=$1
 AGENT_VERSION="v0.9.2"
 LOCAL_IP=`ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'`
+STATUS=255
+SLEEP=5
 echo "### Server   = $HOST_URL"
 echo "### Version  = $AGENT_VERSION"
 echo "### Agent IP = $LOCAL_IP"
 
 # Rancher Agent Installation
+echo "# Debug"
+ps -ef
 echo "# Installing Rancher Agent"
-sudo apt-get install docker -y && sudo docker run -e "CATTLE_AGENT_IP=$LOCAL_IP" -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:$AGENT_VERSION $HOST_URL
+echo "### Installing Docker"
+while [ $STATUS -gt 0 ]
+do
+  sleep $SLEEP
+  sudo apt-get install docker -y
+  STATUS=$?
+done
+echo "### Deploying Rancher Agent as Docker Container"
+sudo docker run -e "CATTLE_AGENT_IP=$LOCAL_IP" -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:$AGENT_VERSION $HOST_URL
+exit $?
