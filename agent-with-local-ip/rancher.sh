52c07cd2f6c3c0e7bdc5b27523f7bca572d877af
Karim Vaes <sourcetree@kvaes.be>
2016-03-03 13:43:59 +0100

    docker install

--- agent-with-local-ip/rancher.sh
+++ agent-with-local-ip/rancher.sh
@@ -13,7 +13,7 @@ else
   echo "### Host Url detected"
 fi
 
-#Setup Variables
+# Variable Setup
 echo "# Setting Variables"
 HOST_URL=$1
 AGENT_VERSION="v0.9.2"
@@ -22,5 +22,9 @@ echo "### Server   = $HOST_URL"
 echo "### Version  = $AGENT_VERSION"
 echo "### Agent IP = $LOCAL_IP"
 
-echo "# Deploying Rancher Agent"
+# Rancher Agent Installation
+echo "# Installing Rancher Agent"
+echo "### Install Docker"
+sudo apt-get install docker -y
+echo "### Deploying Rancher Agent"
 sudo docker run -e "CATTLE_AGENT_IP=$LOCAL_IP" -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:$AGENT_VERSION $HOST_URL
